c4de41f2754cb2084360886cf002b74752a70b46
Anastasiia <blacsharm@gmail.com>
2024-04-12 13:28:05 +0300

    push

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+const SearchBar = ({ onSubmit}) => {
+    return (
+        <header>
+            <form>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+                <button type="submit" onSubmit={onSubmit}>Search</button>
+            </form>
+        </header>
+
+    );
+};
+
+export default SearchBar;
